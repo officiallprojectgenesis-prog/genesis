0d92cf6d7fb71d05da95f06f9fecb36fb7a4f5ee
officiallprojectgenesis-prog <officiall.project.genesis@gmail.com>
2026-03-29 20:19:27 +0400

    Delete [serwer]/[story] directory

--- [serwer]/[story]/story-1/client/effects/main.lua
+++
@@ -1,7 +0,0 @@
-function applyShaderToObject(shader, object, texture)
-    engineApplyShaderToWorldTexture(shader, texture or '*', object)
-    local lod = getLowLODElement(object)
-    if lod then
-        engineApplyShaderToWorldTexture(shader, texture or '*', lod)
-    end
-end
