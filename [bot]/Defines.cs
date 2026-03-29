6f4b6e8429182ee7a59ab190ed20aafebb7c8f5d
officiallprojectgenesis-prog <officiall.project.genesis@gmail.com>
2026-03-29 20:18:47 +0400

    Delete [bot] directory

--- [bot]/Defines.cs
+++
@@ -1,9 +0,0 @@
-public class Defines
-{
-    public static readonly string DiscordToken = "YOUR-DISCORD-TOKEN";
-    public static readonly string MTAToken = "YOUR-MTA-TOKEN";
-    public static readonly string StripeApiKey= "YOUR-STRIPE-TOKEN";
-    public static readonly string MySqlConnectionString = "Server=localhost;Database=mtaserver;Uid=root;Pwd=sherlockholmesxd123;Convert Zero Datetime=True";
-    public static readonly ulong GuildId = 1254413691181334598;
-    public static readonly ulong NotificationsChannelId = 1271868245900525739;
-}
